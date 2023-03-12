915c25090c67da03783ff75c8b2659cc1dca93e1
jklein <jus.klein@icloud.com>
2023-03-12 18:00:40 +0100

    fix(docker-runner): wrong token

--- github-runner/start.sh
+++ github-runner/start.sh
@@ -3,17 +3,17 @@
 GH_OWNER=$GH_OWNER
 GH_REPOSITORY=$GH_REPOSITORY
 GH_TOKEN=$GH_TOKEN
-
-RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
 RUNNER_NAME=$RUNNER_NAME
+RUNNER_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+
 
 cd /home/runner/actions-runner
 
-./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${GH_TOKEN} --name ${RUNNER_NAME}
+./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${RUNNER_TOKEN} --name ${RUNNER_NAME}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${GH_TOKEN}
+    ./config.sh remove --unattended --token ${RUNNER_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
